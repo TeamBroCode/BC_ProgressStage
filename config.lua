fbb0338952d17f9762870286e7bc415f36c6079f
AnishBplayz <anishbplayz@gmail.com>
2024-01-11 14:37:58 +0530

    Update config.lua

--- config.lua
+++ config.lua
@@ -4,8 +4,8 @@ Config = {}
 Config.Core = 'QBCore'      -- QBCore or Standalone
 Config.CoreName = 'qb-core' -- if QBCore then enter your CoreName else leave it false
 
--- Configure support for custom inventories, if applicable
-Config.Inventory = false        -- Set to 'true' for custom inventory support (e.g., qs-inventory)
+-- Configure support for custom inventories, if applicable (Currently Supports qb-inventory, ps-inventory, ox_inventory)
+Config.Inventory = false        -- Set to 'inventoryName' for custom inventory support (e.g., qs-inventory)
 Config.InventoryImgLink = false -- Set a custom image link for the inventory (e.g., nui://ps-inventory/html/)
 
 -- Define default settings for the progress stage
